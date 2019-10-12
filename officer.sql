5107e4d70189e6456c621ba54a952b6c3fa634af
hadia gueye <49209509+hadia15@users.noreply.github.com>
2019-10-12 14:15:03 -0500

    Create officer.sql
    officer database for test

---
+++ officer.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM officerrate.`officer table`
+LIMIT 0, 1000
+
+-- Date: 2019-10-12 14:10
+*/
+INSERT INTO `` (`badgeNumber`,`first name`,`last name`,`city`,`number of stars`,`profil id`) VALUES (123456,'john','smith','minneapolis',3,1234);
+INSERT INTO `` (`badgeNumber`,`first name`,`last name`,`city`,`number of stars`,`profil id`) VALUES (678923,'jane','smith','chicago',2,4566);
