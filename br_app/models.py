6e38eadb3dd8f044bdb56bfa9d47582038e75c20
alekulyn <alekulyn@users.noreply.github.com>
2019-10-12 17:56:16 -0500

    Officers model working.

--- br_app/models.py
+++ br_app/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class Officer(models.Model):
-    officer_id = models.IntegerField()
-    name = models.CharField(max_length=30)
+class Officers(models.Model):
+    badge = models.IntegerField(blank=True, null=True)
+    first = models.CharField(max_length=64, blank=True, null=True)
+    last = models.CharField(max_length=64, blank=True, null=True)
+    city = models.CharField(max_length=64, blank=True, null=True)
+    stars = models.FloatField(blank=True, null=True)
+    profileid = models.IntegerField(primary_key=True)
+
+    class Meta:
+        managed = False
+        db_table = 'officers'
--- br_app/views.py
+++ br_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 #from django.template import loader
 
-from .models import Officer
+from .models import Officers
 
 # Create your views here.
 
@@ -10,7 +10,7 @@ def index(request):
     return HttpResponse("This is the review page.")
 
 def profile(request, officer_id):
-    officer_data = Officer.from_db()
+    officer_data = Officers.from_db()
     context = {'officer_data': officer_data}
     return render(request)
 
@@ -18,4 +18,8 @@ def add_review(request):
     return HttpResponse()
 
 def search(request):
+    template = loader.get_template('results.html')
+    context = {
+            'officers' : None
+    }
     return HttpResponse()
