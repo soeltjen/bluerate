c1b0407d31cf6fd07ea7df2e69a0811af02e5919
Sebastian Oeltjen <sloeltjen@go.stcloudstate.edu>
2019-10-12 14:28:54 -0500

    added app to settings

--- bluerate/settings.py
+++ bluerate/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'review.apps.ReviewConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
