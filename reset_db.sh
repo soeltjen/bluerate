2b4c0fa254fa6bee54aaacb876748c8e5edf50d8
Suhayb Ali <suhaybali@wireless-nat-inside.uiowa.edu>
2019-10-13 06:42:21 -0500

    rounded search box

--- reset_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo mysql -e "DROP DATABASE bluerate"
-sudo mysql -e "CREATE DATABASE bluerate"
-sudo mysql bluerate < officer.txt
-sudo mysql bluerate < user.txt
-sudo mysql bluerate < review.txt
